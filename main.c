821045e5425aa79f523e68a34ab59dd698b9d77a
Masha <m_dvedeva01@mail.ru>
2021-07-13 18:27:13 +0300

    pthread

--- main.c
+++ main.c
@@ -2,63 +2,65 @@
 
 int ft_atoi(const char *s)
 {
- int   i;
- long long num;
- int   min;
+    int   i;
+    long long num;
+    int   min;
 
- i = 0;
- num = 0;
- min = 1;
- while ((s[i] >= 9 && s[i] <= 13) || s[i] == 32)
-  i++;
- if (s[i] == '-' || s[i] == '+')
- {
-  min += -2 * (s[i] == '-');
-  i++;
- }
- while (s[i] >= '0' && s[i] <= '9')
- {
-  num = num * 10 + (s[i] - '0');
-  i++;
- }
- num = num * min;
- return (num);
+    i = 0;
+    num = 0;
+    min = 1;
+    while ((s[i] >= 9 && s[i] <= 13) || s[i] == 32)
+        i++;
+    if (s[i] == '-' || s[i] == '+')
+    {
+        min += -2 * (s[i] == '-');
+        i++;
+    }
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
+    num = num * min;
+    return (num);
 }
 
 int  valid_arg(int argc, char **argv)
 {
- int i;
+    int i;
 
- if (argc < 5 && argc > 6)
-  return (0);
- i = 1;
- while(i < argc - 1)
- {
-  if (ft_atoi(argv[i]) <= 0)
-   return (0);
-  i++;
- }
- return (1);
+    if (argc < 5 && argc > 6)
+        return (0);
+    i = 1;
+    while(i < argc - 1)
+    {
+        if (ft_atoi(argv[i]) <= 0)
+        return (0);
+        i++;
+    }
+    return (1);
 }
 
 int main(int argc, char **argv)
 {
- t_philo philo;
+    t_inter_data inter_data;
 
- if (!valid_arg(argc, argv))
- {
-  printf("Invalid argument!\n");
-  return (0);
- }
- philo.num_philo = ft_atoi(argv[1]);
- philo.time_to_die = ft_atoi(argv[2]);
- philo.time_to_eat = ft_atoi(argv[3]);
- philo.time_to_sleep = ft_atoi(argv[4]);
- if (argc == 6)
-  philo.num_times_eat = ft_atoi(argv[5]);
- printf("num_philo = %d\n", philo.num_philo);
- printf("time_to_die = %d\n", philo.time_to_die);
- printf("time_to_eat = %d\n", philo.time_to_eat);
- printf("time_to_sleep = %d\n", philo.time_to_sleep);
- printf("num_times_eat = %d\n", philo.num_times_eat);
+    if (!valid_arg(argc, argv))
+    {
+        printf("Invalid argument!\n");
+        return (0);
+    }
+    inter_data.num_philo = ft_atoi(argv[1]);
+    inter_data.time_to_die = ft_atoi(argv[2]);
+    inter_data.time_to_eat = ft_atoi(argv[3]);
+    inter_data.time_to_sleep = ft_atoi(argv[4]);
+    if (argc == 6)
+        inter_data.num_times_eat = ft_atoi(argv[5]);
+    inter_data.philos = (t_philos *) malloc(sizeof(t_philos) * inter_data.num_philo);
+    start(&inter_data);
+//  printf("num_philo = %d\n", inter_data.num_philo);
+//  printf("time_to_die = %d\n", inter_data.time_to_die);
+//  printf("time_to_eat = %d\n", inter_data.time_to_eat);
+//  printf("time_to_sleep = %d\n", inter_data.time_to_sleep);
+//  printf("num_times_eat = %d\n", inter_data.num_times_eat);
 }
--- philosophers.h
+++ philosophers.h
@@ -2,14 +2,26 @@
 # define PHILOSOPHERS_H
 # include <unistd.h>
 # include <stdio.h>
+# include <pthread.h>
+#include <stdlib.h>
 
-typedef struct s_philo
+typedef struct s_philos
 {
- int  num_philo;
- int  time_to_die;
- int  time_to_eat;
- int  time_to_sleep;
- int  num_times_eat;
-} t_philo;
+    pthread_t		philo;
+}   t_philos;
+
+typedef struct s_inter_data
+{
+    int     num_philo;
+    int     time_to_die;
+    int     time_to_eat;
+    int     time_to_sleep;
+    int     num_times_eat;
+    t_philos   *philos;
+
+}   t_inter_data;
+
+void	start(t_inter_data *in_data);
+
 
 #endif
